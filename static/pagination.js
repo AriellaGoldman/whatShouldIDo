cd279ae8962b17d26ffd61c27ba4cbbf7f80c899
Daniel Krupka <dkrupkad@gmail.com>
2014-02-02 01:17:14 -0500

    paren qlist

--- static/pagination.js
+++ static/pagination.js
@@ -1,16 +1,30 @@
 function pagination(page_elt) {
 	var page_number = 0;
+  var url_name = "/q?limit=10";
+  
+  $.ajax({
+    url: url_name,
+    success: function(result) {
+      $("#" + page_elt).html(result);
+      $("#" + page_elt + " li").each(function(idx,elem) {
+        elem.innerHTML = (10*page_number + idx + 1) + ". " + elem.innerHTML;
+      });
+    } 
+  });
 
 	this.loadnext = function() {
 		var url_name = "/q?limit=10&offset=" + (10*(page_number + 1));
 		$.ajax({
 			url: url_name,
 			success: function(result) {
+        page_number++;
 				$("#" + page_elt).html(result);
+        $("#" + page_elt + " li").each(function(idx,elem) {
+          elem.innerHTML = (10*page_number + idx + 1) + ". " + elem.innerHTML;
+        });
 			} 
 		});
     
-    page_number++;
 	}
 
 	this.loadprev = function() {
@@ -21,10 +35,12 @@ function pagination(page_elt) {
 		$.ajax({
 			url: url_name,
 			success: function(result) {
+        page_number--;
 				$("#" + page_elt).html(result);
+        $("#" + page_elt + " li").each(function(idx,elem) {
+          elem.innerHTML = (10*page_number + idx + 1) + ". " + elem.innerHTML;
+        });
 			} 
 		});
-  
-    page_number--;
 	}
 }
