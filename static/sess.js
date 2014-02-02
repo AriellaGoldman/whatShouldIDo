75c485077be735c40800bf306453ad4239b3a1e9
Daniel Krupka <dkrupkad@gmail.com>
2014-02-02 04:20:46 -0500

    fixed

---
+++ static/sess.js
@@ -0,0 +1,33 @@
+
+      
+      function showRegister() {
+        $("#popup").load("/s/register").show();
+      }
+      
+      function showLogin() {
+        $("#popup").load("/s/login").show();
+      }
+
+      function showLogout() {
+        $.post("/s/logout");
+        showLogin();
+      }
+      
+      function doLogin() {
+        var arr = [];
+        $("#popup :input").each(function(idx,elem) {
+          if(elem.getAttribute('type') != 'button')
+            arr.push(elem.id + "=" + elem.value);
+        });
+        alert(arr.join('&'));
+        $.post('/s/register?' + arr.join('&'));
+      }
+      
+      function doRegister() {
+        var arr = [];
+        $("#popup :input").each(function(idx,elem) {
+          if(elem.getAttribute('type') != 'button')
+            arr.push(elem.id + "=" + elem.value);
+        });
+        alert(arr.join('&'));
+      }
