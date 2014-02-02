cd3d87682450151782b47f18ab0021cd2f9eaa99
Daniel Krupka <dkrupkad@gmail.com>
2014-02-01 22:06:16 -0500

    js update

---
+++ static/load_qs.js
@@ -0,0 +1,9 @@
+function load_qs(elt_id, offset) {
+	var url_name = "/q?limit=10&offset=" + offset;
+	$.ajax([
+		url: url_name,
+		success: function(result) {
+			$("#" + elt_id).html(result);
+		}
+	]);
+}
