ad5f67e5c2da1d6c8d46fd29faa409c6714bb646
Daniel Krupka <dkrupkad@gmail.com>
2014-02-02 08:14:04 -0500

    fixed

--- static/fblogin.js
+++ static/fblogin.js
@@ -1,4 +1,5 @@
-function fblogin() {
+window.fb {
+  
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '217657201758115',
@@ -7,6 +8,14 @@ function fblogin() {
       xfbml      : true  // parse XFBML
     });
   }
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
   FB.getLoginStatus(function(response) {
     if (response.status === 'connected') {
